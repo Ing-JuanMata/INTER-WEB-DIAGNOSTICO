9b81e122b10ea19e899fc893826de436a6a5c97f
Juan Jesus Mata Solis <jujemataso@ittepic.edu.mx>
2023-01-31 07:56:01 -0700

    INICIO DE PROYECTO

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'diagnostico-9e23a',
+    appId: '1:228079291467:web:c7bb5ddefca6c17c28d1ce',
+    storageBucket: 'diagnostico-9e23a.appspot.com',
+    apiKey: 'AIzaSyDq1aPdUwHOoDWKCHDmDAYk1UDsJFSf8mg',
+    authDomain: 'diagnostico-9e23a.firebaseapp.com',
+    messagingSenderId: '228079291467',
+  },};
